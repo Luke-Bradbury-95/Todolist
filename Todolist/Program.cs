db5601e137489021b0e31ad3b1daf9cc307b7883
luke-bradbury-95 <luke-bradbury-95@hotmail.com>
2024-02-17 13:11:29 +0000

    Added the basics of the TODO and played with some ints and string

--- Todolist/Program.cs
+++ Todolist/Program.cs
@@ -1,2 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userInput = "A";
+Console.WriteLine(userInput);
+
+userInput = "ABC";
+Console.WriteLine(userInput);
+
+
+int number;
+number = 8;
+Console.WriteLine(number);
+
 Console.ReadKey();
