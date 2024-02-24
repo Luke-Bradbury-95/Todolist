909f613bcf97419bf6c9d5b94bbb5050fb569805
luke-bradbury-95 <luke-bradbury-95@hotmail.com>
2024-02-24 17:26:39 +0000

    Deleted pointless stuff

--- Todolist/Program.cs
+++ Todolist/Program.cs
@@ -2,9 +2,9 @@
 using System.Runtime.Intrinsics.X86;
 using System.Xml;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-List<string> list = new List<string>();
-bool exit = false;
-int todoNumber = 1;
+List<string> list = new List<string>(); // list out of loop as to not reset
+bool exit = false; // stopping the app closing after first actions
+int todoNumber = 1; // to keep track of list numbers to help with removal
 while (!exit)
 {
 
@@ -25,7 +25,7 @@ while (!exit)
                 Console.WriteLine("No To-Do's");
             }
             else {
-                foreach (string word in list)
+                foreach (string word in list) // listing all list items
                 {
                     Console.WriteLine(word);
                 }
@@ -36,7 +36,7 @@ while (!exit)
         case "a":
             PrintSelectedOption("Add all TODOs");
             var input = Console.ReadLine();
-            if (!string.IsNullOrEmpty(input))
+            if (!string.IsNullOrEmpty(input)) // Making sure app doesnt crash if nothing is added
             {
                 list.Add($"{todoNumber}){input}");
                 Console.Write("ToDo Added");
@@ -82,10 +82,10 @@ while (!exit)
             {
                 Console.WriteLine("Selected option: " + selectedOption);   //Implementing this method to avoid using the same line of code too many times
             }
-            static void RenumberToDos(List<string> list)
+            static void RenumberToDos(List<string> list) // if a list from a middle row is removed, the list will renumber itself
             {
                 for (int i = 0; i < list.Count; i++)
                 {
-                    list[i] = $"{i + 1}: {list[i].Substring(list[i].IndexOf(':') + 2)}";
+                    list[i] = $"{i + 1}: {list[i].Substring(list[i].IndexOf(':') + 2)}"; 
                 }
             }
